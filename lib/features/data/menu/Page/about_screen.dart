8dac3d789bbf30b5b53abdaf81f5799de6e79c64
01001000 00101110 01 <hosseinmohammadi.dev22@yahoo.com>
2025-07-19 10:32:42 +0330

    Edit AboutScreen

--- lib/features/data/menu/Page/about_screen.dart
+++ lib/features/data/menu/Page/about_screen.dart
@@ -1,5 +1,8 @@
 import 'package:autilab_project/common/widgets/appbar_back_screen.dart';
+import 'package:autilab_project/core/constants/color_constant.dart';
+import 'package:autilab_project/core/constants/theme_constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../../../../utils/functions/animation_control.dart';
 
@@ -44,8 +47,50 @@ class _AboutScreenState extends State<AboutScreen>
             context: context, title: 'About AutiLab', isIcon: true),
         body: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              children: [],
+            child: Padding(
+              padding: AutilabMargin.marginFullScreen,
+              child: Column(
+                // spacing: 48,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(
+                    height: 32,
+                  ),
+                  SvgPicture.asset(
+                    'assets/images/about_image.svg',
+                    height: 180,
+                    width: 350,
+                    // fit: BoxFit.cover,
+                  ),
+                  const SizedBox(
+                    height: 48,
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      style: AutilabTextStyle.small16_400.copyWith(
+                        color: AutilabColor.black,
+                      ),
+                      children: [
+                        const TextSpan(
+                          text:
+                              "AutiLab was created with one simple belief — that every child on the autism spectrum deserves care that truly understands them. It’s a space where differences are understood, and every unique journey is respected. Designed to support children with autism and their families, the app offers accessible, evidence-based tools for early intervention, personalized learning, and ongoing progress tracking.Our goal is to empower parents, therapists, and educators with practical resources that truly make a difference in each child's development journey. ",
+                        ),
+                        WidgetSpan(
+                          child: SvgPicture.asset(
+                            'assets/icons/redheart.svg',
+                            width: 16,
+                            height: 16,
+                          ),
+                          alignment: PlaceholderAlignment.bottom,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 48,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
