118af55c906a1ad9016bfe5043f757fe0f6dc6ab
01001000 00101110 01 <hosseinmohammadi.dev22@yahoo.com>
2025-07-23 01:15:49 +0330

    Edit MySubscribtionScreen

--- lib/features/data/menu/widgets/packagessubscribtion.dart
+++ lib/features/data/menu/widgets/packagessubscribtion.dart
@@ -13,68 +13,91 @@ class PackagesSubscribtion extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:
-          appBarWidget(context: context, title: 'Subscribtion', isIcon: true),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              ListView.builder(
-                shrinkWrap: true,
-                padding: AutilabMargin.marginFullScreen,
-                itemCount: titlePlanList.length,
-                physics: const NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  child: PlanBoxWidget(
-                    title: titlePlanList[index],
-                    description: descriptionPlanList[index],
-                    titleButton: 'Choose',
-                    widget: const [
-                      PalnItemWidget(
-                        title: 'Full access to the tools section',
-                      ),
-                      PalnItemWidget(
-                        title: 'Access to aptitude test',
-                      ),
-                      PalnItemWidget(
-                        title: 'Access to personality test',
-                      ),
-                      PalnItemWidget(
-                        title: 'Unlimited appointment booking',
-                      ),
-                      PalnItemWidget(
-                        title: 'Access to nearby centers',
-                      ),
-                      PalnItemWidget(
-                        title: 'Doctor appointment booking discount',
-                      ),
-                    ],
-                    onTap: () {
-                      int price = 0;
-                      if (index == 1) {
-                        price = 99;
-                      } else {
-                        price = 299;
-                      }
-                      context.pushNamed(
-                        AutiLabRoutes.payPackagesScreen,
-                        extra: {
-                          'title': titlePlanList[index],
-                          'description': descriptionPlanList[index],
-                          'price': price,
-                        },
-                      );
-                    },
-                    margin: 0,
-                  ),
-                ),
-              ),
-            ],
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        bool isMobile() {
+          if (constraints.maxWidth < 600) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+
+        return Scaffold(
+          appBar: appBarWidget(
+            context: context,
+            title: 'Subscribtion',
+            isIcon: true,
+            isMobile: isMobile(),
           ),
-        ),
-      ),
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  ListView.builder(
+                    shrinkWrap: true,
+                    padding: AutilabMargin.marginFullScreen,
+                    itemCount: titlePlanList.length,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) => Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      child: PlanBoxWidget(
+                        isMobile: isMobile(),
+                        title: titlePlanList[index],
+                        description: descriptionPlanList[index],
+                        titleButton: 'Choose',
+                        widget: [
+                          PalnItemWidget(
+                            isMobile: isMobile(),
+                            title: 'Full access to the tools section',
+                          ),
+                          PalnItemWidget(
+                            isMobile: isMobile(),
+                            title: 'Access to aptitude test',
+                          ),
+                          PalnItemWidget(
+                            isMobile: isMobile(),
+                            title: 'Access to personality test',
+                          ),
+                          PalnItemWidget(
+                            isMobile: isMobile(),
+                            title: 'Unlimited appointment booking',
+                          ),
+                          PalnItemWidget(
+                            isMobile: isMobile(),
+                            title: 'Access to nearby centers',
+                          ),
+                          PalnItemWidget(
+                            isMobile: isMobile(),
+                            title: 'Doctor appointment booking discount',
+                          ),
+                        ],
+                        onTap: () {
+                          int price = 0;
+                          if (index == 1) {
+                            price = 99;
+                          } else {
+                            price = 299;
+                          }
+                          context.pushNamed(
+                            AutiLabRoutes.payPackagesScreen,
+                            extra: {
+                              'title': titlePlanList[index],
+                              'description': descriptionPlanList[index],
+                              'price': price,
+                            },
+                          );
+                        },
+                        margin: 0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
