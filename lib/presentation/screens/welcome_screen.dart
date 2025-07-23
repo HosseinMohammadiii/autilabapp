64390b55508e39e65a1a089db99788d12ab7e022
01001000 00101110 01 <hosseinmohammadi.dev22@yahoo.com>
2025-07-23 14:09:37 +0330

    Edit WelcomeScreen

--- lib/presentation/screens/welcome_screen.dart
+++ lib/presentation/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:autilab_project/common/widgets/appbar_back_screen.dart';
 import 'package:autilab_project/common/widgets/custom_button_widget.dart';
 import 'package:autilab_project/core/constants/color_constant.dart';
+import 'package:autilab_project/core/constants/theme_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
@@ -41,99 +42,116 @@ class _WelcomeScreenState extends State<WelcomeScreen>
 
   @override
   Widget build(BuildContext context) {
-    return FadeTransition(
-      opacity: animationHelper.fadeAnimation,
-      child: Scaffold(
-        appBar: appBarWidget(context: context, title: 'Back', isIcon: true),
-        body: SafeArea(
-          child: CustomScrollView(
-            slivers: [
-              SliverFillRemaining(
-                hasScrollBody: false,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const SizedBox(
-                      height: 64,
-                    ),
-                    SvgPicture.asset(
-                      'assets/images/welcome_image.svg',
-                      height: 242,
-                      width: 306,
-                      fit: BoxFit.contain,
-                    ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    Text(
-                      'Welcome To Autilab!',
-                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        bool isMobile() {
+          if (constraints.maxWidth < 600) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+
+        return FadeTransition(
+          opacity: animationHelper.fadeAnimation,
+          child: Scaffold(
+            appBar: appBarWidget(
+              context: context,
+              title: 'Back',
+              isIcon: true,
+              isMobile: isMobile(),
+            ),
+            body: SafeArea(
+              child: CustomScrollView(
+                slivers: [
+                  SliverFillRemaining(
+                    hasScrollBody: false,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          height: 64,
+                        ),
+                        SvgPicture.asset(
+                          'assets/images/welcome_image.svg',
+                          height: isMobile() ? 242 : 540,
+                          width: isMobile() ? 306 : 574,
+                          fit: BoxFit.contain,
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        Text(
+                          'Welcome To Autilab!',
+                          style: AutilabTextStyle.medium20_500.copyWith(
+                            fontSize: isMobile() ? 20 : 32,
                           ),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Text(
-                        'Big support. Bright smiles. Welcome to AutiLab!',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(
+                            'Big support. Bright smiles. Welcome to AutiLab!',
+                            textAlign: TextAlign.center,
+                            style: AutilabTextStyle.small16_400.copyWith(
+                              fontSize: isMobile() ? 16 : 28,
                             ),
-                      ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 24,
+                        ),
+                        CustomButtonWidget(
+                          isMobile: isMobile(),
+                          onTap: () {
+                            context.pushNamed(
+                              AutiLabRoutes.loginScreen,
+                            );
+                          },
+                          height: 50,
+                          borderRadius: 16,
+                          color: AutilabColor.bb,
+                          text: 'Login',
+                          textStyle:
+                              Theme.of(context).textTheme.bodySmall!.copyWith(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                        ),
+                        const SizedBox(
+                          height: 16,
+                        ),
+                        CustomButtonWidget(
+                          isMobile: isMobile(),
+                          onTap: () {
+                            context.pushNamed(
+                              AutiLabRoutes.signUpScreen,
+                            );
+                          },
+                          height: 50,
+                          color: AutilabColor.white,
+                          text: 'Sign Up',
+                          width: 70,
+                          margin: const EdgeInsets.all(0),
+                          textStyle:
+                              Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w600,
+                                    color: AutilabColor.blue,
+                                  ),
+                        ),
+                      ],
                     ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    CustomButtonWidget(
-                      onTap: () {
-                        context.pushNamed(
-                          AutiLabRoutes.loginScreen,
-                        );
-                      },
-                      height: 50,
-                      borderRadius: 16,
-                      color: AutilabColor.bb,
-                      text: 'Login',
-                      textStyle:
-                          Theme.of(context).textTheme.bodySmall!.copyWith(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w400,
-                              ),
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    CustomButtonWidget(
-                      onTap: () {
-                        context.pushNamed(
-                          AutiLabRoutes.signUpScreen,
-                        );
-                      },
-                      height: 50,
-                      color: AutilabColor.white,
-                      text: 'Sign Up',
-                      width: 70,
-                      margin: const EdgeInsets.all(0),
-                      textStyle:
-                          Theme.of(context).textTheme.bodyMedium!.copyWith(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
-                                color: AutilabColor.blue,
-                              ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
