caf7964fdbc68305da95692a3b5c8a6970fe3ca8
01001000 00101110 01 <hosseinmohammadi.dev22@yahoo.com>
2025-07-24 11:29:03 +0330

    Edit AboutScreen

--- lib/features/data/menu/Page/about_screen.dart
+++ lib/features/data/menu/Page/about_screen.dart
@@ -39,44 +39,61 @@ class _AboutScreenState extends State<AboutScreen>
 
   @override
   Widget build(BuildContext context) {
-    return FadeTransition(
-      opacity: animationHelper.fadeAnimation,
-      child: Scaffold(
-        appBar: appBarWidget(
-            context: context, title: 'About AutiLab', isIcon: true),
-        body: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: AutilabMargin.marginFullScreen,
-              child: Column(
-                // spacing: 48,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  const SizedBox(
-                    height: 32,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        bool isMobile() {
+          if (constraints.maxWidth < 600) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+
+        return FadeTransition(
+          opacity: animationHelper.fadeAnimation,
+          child: Scaffold(
+            appBar: appBarWidget(
+              context: context,
+              title: 'About AutiLab',
+              isIcon: true,
+              isMobile: isMobile(),
+            ),
+            body: SafeArea(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: AutilabMargin.marginFullScreen,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      SvgPicture.asset(
+                        'assets/images/about_image.svg',
+                        height: isMobile() ? 180 : 360,
+                        width: isMobile() ? 350 : 700,
+                        // fit: BoxFit.cover,
+                      ),
+                      const SizedBox(
+                        height: 48,
+                      ),
+                      Text(
+                        "AutiLab was created with one simple belief — that every child on the autism spectrum deserves care that truly understands them. It’s a space where differences are understood, and every unique journey is respected. Designed to support children with autism and their families, the app offers accessible, evidence-based tools for early intervention, personalized learning, and ongoing progress tracking.Our goal is to empower parents, therapists, and educators with practical resources that truly make a difference in each child's development journey. ",
+                        style: AutilabTextStyle.small16_400.copyWith(
+                          fontSize: isMobile() ? 16 : 28,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 48,
+                      ),
+                    ],
                   ),
-                  SvgPicture.asset(
-                    'assets/images/about_image.svg',
-                    height: 180,
-                    width: 350,
-                    // fit: BoxFit.cover,
-                  ),
-                  const SizedBox(
-                    height: 48,
-                  ),
-                  Text(
-                    "AutiLab was created with one simple belief — that every child on the autism spectrum deserves care that truly understands them. It’s a space where differences are understood, and every unique journey is respected. Designed to support children with autism and their families, the app offers accessible, evidence-based tools for early intervention, personalized learning, and ongoing progress tracking.Our goal is to empower parents, therapists, and educators with practical resources that truly make a difference in each child's development journey. ",
-                    style: AutilabTextStyle.small16_400,
-                  ),
-                  const SizedBox(
-                    height: 48,
-                  ),
-                ],
+                ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
