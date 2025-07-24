8990be716f13248b14461448be58d9091a70cc6b
01001000 00101110 01 <hosseinmohammadi.dev22@yahoo.com>
2025-07-24 09:26:15 +0330

    Edit SuccessfulSubmitRateScreen

--- lib/features/data/menu/Page/successful_submit_rate_screen.dart
+++ lib/features/data/menu/Page/successful_submit_rate_screen.dart
@@ -5,59 +5,110 @@ import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../common/widgets/custom_button_widget.dart';
+import '../../../../utils/functions/animation_control.dart';
 
-class SuccessfulSubmitRateScreen extends StatelessWidget {
+class SuccessfulSubmitRateScreen extends StatefulWidget {
   const SuccessfulSubmitRateScreen({super.key});
 
   @override
+  State<SuccessfulSubmitRateScreen> createState() =>
+      _SuccessfulSubmitRateScreenState();
+}
+
+class _SuccessfulSubmitRateScreenState extends State<SuccessfulSubmitRateScreen>
+    with TickerProviderStateMixin {
+  late AnimationHelper animationHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    animationHelper = AnimationHelper(
+        vsync: this, begin: 0.5, duration: const Duration(seconds: 1));
+    animationHelper.animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationHelper.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant SuccessfulSubmitRateScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    animationHelper.restartAnimation();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 128,
-              height: 128,
-              margin: const EdgeInsets.only(bottom: 48),
-              decoration: const BoxDecoration(
-                color: AutilabColor.yellow,
-                shape: BoxShape.circle,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        bool isMobile() {
+          if (constraints.maxWidth < 600) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+
+        return FadeTransition(
+          opacity: animationHelper.fadeAnimation,
+          child: Scaffold(
+            body: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: isMobile() ? 128 : 252,
+                    height: isMobile() ? 128 : 252,
+                    margin: const EdgeInsets.only(bottom: 48),
+                    decoration: const BoxDecoration(
+                      color: AutilabColor.yellow,
+                      shape: BoxShape.circle,
+                    ),
+                    child: SvgPicture.asset(
+                      'assets/icons/tik.svg',
+                      fit: BoxFit.scaleDown,
+                    ),
+                  ),
+                  Text(
+                    'Thank You For Your Feedback!',
+                    textAlign: TextAlign.center,
+                    style: AutilabTextStyle.medium20_500.copyWith(
+                      fontSize: isMobile() ? 20 : 24,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    "We're always working to serve you better.",
+                    textAlign: TextAlign.center,
+                    style: AutilabTextStyle.small16_400.copyWith(
+                      color: AutilabColor.gray,
+                      fontSize: isMobile() ? 16 : 20,
+                    ),
+                  ),
+                  CustomButtonWidget(
+                    isMobile: isMobile(),
+                    onTap: () {
+                      context.pop();
+                      context.pop();
+                    },
+                    height: 50,
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 32),
+                    color: AutilabColor.bb,
+                    text: 'OK ',
+                    textStyle: AutilabTextStyle.small18_400,
+                  ),
+                ],
               ),
-              child: SvgPicture.asset(
-                'assets/icons/tik.svg',
-                fit: BoxFit.scaleDown,
-              ),
             ),
-            Text(
-              'Thank You For Your Feedback!',
-              textAlign: TextAlign.center,
-              style: AutilabTextStyle.medium20_500,
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            Text(
-              "We're always working to serve you better.",
-              textAlign: TextAlign.center,
-              style: AutilabTextStyle.small16_400
-                  .copyWith(color: AutilabColor.gray),
-            ),
-            CustomButtonWidget(
-              onTap: () {
-                context.pop();
-                context.pop();
-              },
-              height: 50,
-              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 32),
-              color: AutilabColor.bb,
-              text: 'OK ',
-              textStyle: AutilabTextStyle.small18_400,
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
